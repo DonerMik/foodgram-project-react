e027331159578b6ce50d20a10c70b6605d4e91d7
donermik <denismi92@gmail.com>
2022-09-11 21:29:20 +0300

    first commit

--- backend/api_recipes/api_recipes/settings.py
+++ backend/api_recipes/api_recipes/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 
 
-SECRET_KEY = 'django-insecure-mh%ka$du9=fj@+!lpvu^*5-&-so$5@mg&w9tbqdfo++h#y!azy'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 DEBUG = False
@@ -73,15 +73,11 @@ WSGI_APPLICATION = 'api_recipes.wsgi.application'
 
 DATABASES = {
     'default': {
-            'ENGINE': os.getenv(
-                'DB_ENGINE',
-                default='django.db.backends.postgresql'),
-            'NAME': os.getenv('DB_NAME', default='postgres'),
-            'USER': os.getenv('POSTGRES_USER', default='postgres'),
-            'PASSWORD': os.getenv(
-                'POSTGRES_PASSWORD',
-                default='postgres'),
-            'HOST': os.getenv('DB_HOST', default='db'),
+            'ENGINE': os.getenv('DB_ENGINE'),
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('POSTGRES_USER'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
             'PORT': os.getenv('DB_PORT')
     }
 }
