e977ec592de5f8c6149ef9b35c4810ea60c1ca2a
donermik <denismi92@gmail.com>
2022-09-12 21:19:50 +0300

    first commit

--- backend/api_recipes/api_recipes/settings.py
+++ backend/api_recipes/api_recipes/settings.py
@@ -17,11 +17,6 @@ SECRET_KEY = os.getenv(
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-    # '127.0.0.1',
-    # 'localhost',
-    # '51.250.106.186'
-# ]
-
 
 INSTALLED_APPS = [
     'django.contrib.admin',
